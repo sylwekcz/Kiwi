d161d7c4060851fe9af11440e2ca577e2453e5f1
NX49 <intelextreme@hotmail.com>
2015-05-14 01:51:04 +0200

    insert, update, delete

--- includes/database.php
+++ includes/database.php
@@ -10,6 +10,11 @@ use \UnexpectedValueException;
 register_shutdown_function(__NAMESPACE__ . '\Database::disconnect');
 
 
+/**
+ * Database access and management class
+ *
+ * @package Kiwi
+ */
 abstract class Database
 {
 	/** @var mysqli Mysqli connection handle */
@@ -52,17 +57,17 @@ abstract class Database
 	/**
 	 * Execute insert query
 	 *
-	 * @param string $tableName  Target table
-	 * @param array  $data       Data array consisting of field and value
+	 * @param string $table Target table
+	 * @param array  $data  Data array consisting of field and value
 	 *
-	 * @return bool True on successfully inserted record, false otherwise
+	 * @return bool ID of inserted record on success (positive values only), false otherwise
 	 */
-	final public static function insert($tableName, $data)
+	final public static function insert($table, $data)
 	{
-		if (!is_string($tableName) || !is_array($data))
+		if (!is_string($table) || !is_array($data) && !empty($data))
 			throw new InvalidArgumentException;
 
-		if (!self::isValidName($tableName))
+		if (!self::isValidName($table))
 			throw new UnexpectedValueException;
 
 		if (!self::isConnected())
@@ -73,9 +78,9 @@ abstract class Database
 		list ($fields, $placeholders, $values, $format) = self::_prepareData($data);
 		array_unshift($values, $format); // Prepare for bind_param where format is the first param
 
-		$statement = self::$_handle->prepare("INSERT INTO {$tableName} ({$fields}) VALUES ({$placeholders})");
+		$statement = self::$_handle->prepare("INSERT INTO {$table} ({$fields}) VALUES ({$placeholders})");
 
-		// Formatting failed somehow
+		// Bad query
 		if (!$statement)
 			throw new RuntimeException;
 
@@ -86,26 +91,112 @@ abstract class Database
 		if (!is_callable($handler) || !call_user_func_array($handler, array_references($values)))
 			throw new RuntimeException;
 
+		// Query failed
+		if (!$statement->execute())
+		{
+			// Duplicate record?
+			if ($statement->errno == 1062)
+				return false;
 
-		return $statement->execute() && $statement->affected_rows;
+			throw new RuntimeException;
+		}
+
+		// Something went wrong
+		if (!$statement->affected_rows)
+			return false;
+
+
+		return $statement->insert_id;
 	}
 
 	/**
 	 * Execute update query
 	 *
-	 * @param string $tableName Target table
-	 * @param array $data
-	 * @param array $where
+	 * @param string $table      Target table
+	 * @param array  $data       New data
+	 * @param array  $conditions Update conditions
+	 *
+	 * @return bool True on successfully updated record (keep in mind that it might be zero), false otherwise
 	 */
-	final public static function update($tableName, $data, $where)
+	final public static function update($table, $data, $conditions)
 	{
-		if (!is_string($tableName) || !is_array($data) || !is_string($where))
+		if (!is_string($table) || !is_array($data) || !is_array($conditions))
 			throw new InvalidArgumentException;
+
+		if (empty($data) || empty($conditions))
+			throw new UnexpectedValueException;
+
+		if (!self::isValidName($table))
+			throw new UnexpectedValueException;
+
+		if (!self::isConnected())
+			throw new DatabaseNotConnected;
+
+
+		// Prepare data and conditions
+		list ($dataPlaceholders, $dataValues, $dataFormat) = self::_prepareData($data, true);
+		list ($conditions, $conditionValues, $conditionFormat) = self::_prepareConditions($conditions);
+
+		// Merge values and prepare for bind_param
+		$values = array_merge($dataValues, $conditionValues);
+		$format = $dataFormat . $conditionFormat;
+		array_unshift($values, $format);
+
+		$statement = self::$_handle->prepare("UPDATE {$table} SET {$dataPlaceholders} WHERE {$conditions}");
+
+		// Bad query
+		if (!$statement)
+			throw new RuntimeException;
+
+
+		$handler = [$statement, 'bind_param'];
+
+		// Pass parameters
+		if (!is_callable($handler) || !call_user_func_array($handler, array_references($values)) || !$statement->execute())
+			throw new RuntimeException;
+
+
+		return $statement->affected_rows;
 	}
 
-	final public static function delete($table, $where, $whereFormat)
+	/**
+	 * Execute delete query
+	 *
+	 * @param string $table      Target table
+	 * @param array  $conditions Delete conditions
+	 *
+	 * @return bool|int Number of deleted records on success (keep in mind that it might be zero), false otherwise
+	 */
+	final public static function delete($table, $conditions)
 	{
+		if (!is_string($table) || !is_array($conditions))
+			throw new InvalidArgumentException;
 
+		if (empty($conditions))
+			throw new UnexpectedValueException;
+
+		if (!self::isValidName($table))
+			throw new UnexpectedValueException;
+
+
+		list ($conditions, $values, $format) = self::_prepareConditions($conditions);
+		array_unshift($values, $format);
+
+		$statement = self::$_handle->prepare("DELETE FROM {$table} WHERE {$conditions}");
+
+		// Bad query
+		if (!$statement)
+			throw new RuntimeException;
+
+
+		$handler = [$statement, 'bind_param'];
+
+		// Pass parameters
+		if (!is_callable($handler) || !call_user_func_array($handler, array_references($values)) || !$statement->execute())
+			throw new RuntimeException;
+
+
+		return $statement->affected_rows;
 	}
 
 	final public static function select()
@@ -113,6 +204,15 @@ abstract class Database
 
 	}
 
+	/**
+	 * Parse data for later (easier) use with bind_param
+	 * Supports Update queries
+	 *
+	 * @param array $data Input data array
+	 * @param bool $update Switch to Update queries, fields column will be omitted
+	 *
+	 * @return array Parsed data as fields, placeholders, values and values format
+	 */
 	final private static function _prepareData($data, $update = false)
 	{
 		if (!is_array($data))
@@ -130,45 +230,61 @@ abstract class Database
 			foreach ($data as $field => $value)
 			{
 				// Validate type and build format
-				if (is_int($value))         $format .= 'i';
+				if (is_int($value)) $format .= 'i';
 				else if (is_string($value)) $format .= 's';
 				else if (is_double($value)) $format .= 'd';
 				else
 					throw new UnexpectedValueException;
 
 
-				$fields .= $field . ', '; // Build field list
-				array_push($values, $value); // Build value list
+				if ($update)
+					$placeholders .= $field . '=?, '; // Column name is required when updating
+				else
+				{
+					$fields .= $field . ', '; // Build field list
+					$placeholders .= '?, ';
+				}
 
-				// Column name is required when updating
-				if ($update) $placeholders .= $field . '=?, ';
-				else         $placeholders .= '?, ';
+				array_push($values, $value); // Build value list
 			}
 
 			// Remove last comma space sequence
-			$fields       = substr($fields, 0, -2);
+			if (!$update)
+				$fields = substr($fields, 0, -2);
+
 			$placeholders = substr($placeholders, 0, -2);
 		}
+
+		if ($update)
+			return [$placeholders, $values, $format];
 
 		return [$fields, $placeholders, $values, $format];
 	}
 
-	final public static function _prepareConditions($conditionData)
+	/**
+	 * Parse conditions for later (easier) use with bind_param
+	 * Supported operators: null(IS NULL) = < > >= <= <> %(LIKE) !%(NOT LIKE)
+	 *
+	 * @param array $conditions Input data
+	 *
+	 * @return array Parsed data as criteria, values, and values format
+	 */
+	final public static function _prepareConditions($conditions)
 	{
-		if (!is_array($conditionData))
+		if (!is_array($conditions))
 			throw new InvalidArgumentException;
 
 
-		$conditions = ''; // Output SQL query
+		$criteria = ''; // Output SQL query
 		$values     = [];
 		$format     = ''; // Values format
 
-		$inside     = false; // Condition nesting
+		$inside = false; // Condition nesting
 
 
-		if (!empty($conditionData))
+		if (!empty($conditions))
 		{
-			foreach ($conditionData as $field => $data)
+			foreach ($conditions as $field => $data)
 			{
 				// Nested condition, ie: ['a' => ['b' => 'c', 'd' => 1]]
 				if (is_int($field) && is_array($data))
@@ -177,12 +293,16 @@ abstract class Database
 
 					// Parse nested condition, this means we are building an alternative
 					list ($nestedFields, $nestedValues, $nestedFormat) = self::_prepareConditions($data);
-					$conditions .= '(' . $nestedFields . ') OR ';
-					$values     = array_merge($values, $nestedValues); // Copy values
-					$format     .= $nestedFormat; // Copy format
+					$criteria .= '(' . $nestedFields . ') OR ';
+					$values = array_merge($values, $nestedValues); // Copy values
+					$format .= $nestedFormat; // Copy format
 
 					continue;
 				}
+
+
+				if (!self::isValidName($field))
+					throw new UnexpectedValueException;
 
 
 				// Extract value
@@ -191,13 +311,13 @@ abstract class Database
 				// Convert extra operators
 				if (is_null($value))
 				{
-					$conditions .= $field . ' IS NULL AND ';
+					$criteria .= $field . ' IS NULL AND ';
 					continue;
 				}
 
 
 				// Validate type and build format
-				if (is_int($value))         $format .= 'i';
+				if (is_int($value)) $format .= 'i';
 				else if (is_string($value)) $format .= 's';
 				else if (is_double($value)) $format .= 'd';
 				else
@@ -208,28 +328,28 @@ abstract class Database
 				$operator = is_array($data) ? $data[0] : '=';
 
 				// Operators allowed: null = != > < !< !> >= <= <> % !%
-				if (!preg_match('/^( |=|!=|>|<|!<|!>|>=|<=|<>|%|!%)$/', $operator))
+				if (!preg_match('/^( |=|!=|>|<|>=|<=|<>|%|!%)$/', $operator))
 					throw new InvalidArgumentException;
 
 
-				if ($operator === '%')       $operator = 'LIKE';
+				if ($operator === '%') $operator = 'LIKE';
 				else if ($operator === '!%') $operator = 'NOT LIKE';
 
-				$conditions .= $field . ' ' . $operator . ' ? AND ';
+				$criteria .= $field . ' ' . $operator . ' ? AND ';
 				array_push($values, $value);
 			}
 
 			// Cut the last AND statement
 			if (!$inside)
-				$conditions = substr($conditions, 0, -5);
+				$criteria = substr($criteria, 0, -5);
 		}
 
 		// Cut the last OR statement
 		if ($inside)
-			$conditions = substr($conditions, 0, -4);
+			$criteria = substr($criteria, 0, -4);
 
 
-		return [$conditions, $values, $format];
+		return [$criteria, $values, $format];
 	}
 
 
@@ -256,13 +376,28 @@ abstract class Database
 	}
 
 
-	final private static function isValidName($tableName)
+	/**
+	 * Make sure given string contains only safe (word) characters
+	 *
+	 * @param string $name Input string
+	 *
+	 * @return bool True if string is safe, false otherwise
+	 */
+	final private static function isValidName($name)
 	{
-		return !preg_match('/(\W)/', $tableName);
+		return is_string($name) && !preg_match('/(\W)/', $name);
 	}
 }
 
-class DatabaseFailedException          extends RuntimeException { }
-class DatabaseNotConnected             extends RuntimeException { }
 
-class DatabaseConditionSyntaxException extends RuntimeException { }
+class DatabaseFailedException extends RuntimeException
+{
+}
+
+class DatabaseNotConnected extends RuntimeException
+{
+}
+
+class DatabaseConditionSyntaxException extends RuntimeException
+{
+}
--- index.php
+++ index.php
@@ -10,21 +10,11 @@ require_once('./includes/common.php');
 
 
 Database::connect();
-echo Database::insert('sale', ['numer_sali' => '1A', 'ilosc_miejsc' => 40], 'si') ? 'dodano' : 'duplikat';
+var_dump(Database::delete('sale', ['sala_id' => ['>', 0]]));
+var_dump(Database::insert('sale', ['numer_sali' => '1D', 'ilosc_miejsc' => 10]));
+var_dump($a = Database::insert('sale', ['numer_sali' => '1A', 'ilosc_miejsc' => 11]));
+var_dump(Database::insert('sale', ['numer_sali' => '1B', 'ilosc_miejsc' => 40]));
+var_dump($b = Database::insert('sale', ['numer_sali' => '99C', 'ilosc_miejsc' => 10]));
 
-
-var_dump(Database::_prepareConditions(
-		[
-				[
-						'a' => 10,
-						'b' => ['!%', 'aaaa']
-				],
-				[
-						['a' => ['!=', 10]],
-						[['b' => ['%', 'bbbb']],
-						['t' => null]]
-				],
-				['a' =>
-						 ['=' , 1]
-				]
-		]));
+var_dump(Database::update('sale', ['ilosc_miejsc' => 51], ['numer_sali' => '1D']));
+var_dump(Database::delete('sale', [['ilosc_miejsc' => ['>', 11]], ['sala_id' => $a], ['sala_id' => $b]]));
